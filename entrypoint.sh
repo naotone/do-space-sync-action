2a89a68fd886ae91ec4732534799d7e753ee6a1f
Naoto Yoshimoto <naotone@floatingdots.com>
2020-12-03 14:40:42 -0500

    Remove delete option

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ aws_secret_access_key = ${SPACE_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 sync ${SOURCE_DIR} s3://${SPACE_NAME} \
             --follow-symlinks \
-            --delete \
             --endpoint https://${SPACE_REGION}.digitaloceanspaces.com $*
 
 rm -rf ~/.aws
